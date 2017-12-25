709f1944159cf6ddafa55f402e768432bc01c970
Rajeshkrsna <32624506+Rajeshkrsna@users.noreply.github.com>
2017-12-25 11:58:22 -0500

    updated design files.

--- Comic-Books-Gallery/Comic-Books-Gallery/Controllers/ComicBooksController.cs
+++ Comic-Books-Gallery/Comic-Books-Gallery/Controllers/ComicBooksController.cs
@@ -12,6 +12,21 @@ namespace Comic_Books_Gallery.Controllers
      
         public ActionResult Details()
         {
+            //ViewBag is an object provided by MVC, to pass data from controlle rto view.
+            // but it is dynamic type. and viewbag properties are not case senstive. 
+            //capitalising first letter is just naming convention.
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();
         }
         public  ActionResult Index()
